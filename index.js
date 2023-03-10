0d15eebbe189d4ae74b1ca68d545febd15c3ec8a
Zaky fetoh <zaky.fetoh@gmail.com>
2023-03-10 19:31:21 +0200

    update

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-const express = 
+const express = require("express")
+const morgan = require("morgan")
 
+const PORT = 3000 
+
+const app = express()
+    .use(morgan("combined"))
+    .get("/",(req, res)=>{
+    res.json({
+        hello: 'Ahmed'
+    })})
+    .get("/maklad", (req ,res)=>{
+        res.status(200).json({
+            name:"mohmed"
+        })})
+    .post("/moh", (req ,res)=>{
+        res.json({
+            p: req.most,
+            type:"post"
+        })
+    })
+    .get("/moh",(req, res)=>{
+        res.json({
+            type:"get"
+        })
+    })
+
+    const ser = app.listen(PORT, ()=>{
+    console.log("Server is listening at :", ser.address().port);
+})
